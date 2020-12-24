f62a069433c61e42b68780144b8d6cce81c360f9
clszzyh <clszzyh@gmail.com>
2020-12-24 12:08:40 +0800

    add ci
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # BattleCityCore
 
-**TODO: Add description**
+[![ci](https://github.com/clszzyh/battle_city_core/workflows/ci/badge.svg)](https://github.com/clszzyh/battle_city_core/actions)
+[![Hex.pm](https://img.shields.io/hexpm/v/battle_city_core)](http://hex.pm/packages/battle_city_core)
+[![Hex.pm](https://img.shields.io/hexpm/dt/battle_city_core)](http://hex.pm/packages/battle_city_core)
+[![Documentation](https://img.shields.io/badge/hexdocs-latest-blue.svg)](https://hexdocs.pm/battle_city_core/readme.html)
+
+<!-- MDOC -->
+
+Battle City Core.
+
+<!-- MDOC -->
 
 ## Installation
 
@@ -18,4 +27,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/battle_city_core](https://hexdocs.pm/battle_city_core).
-
--- mix.exs
+++ mix.exs
@@ -1,13 +1,60 @@
 defmodule BattleCityCore.MixProject do
   use Mix.Project
 
+  @version "VERSION" |> File.read!() |> String.trim()
+  @github_url "https://github.com/clszzyh/battle_city_core"
+  @description "README.md"
+               |> File.read!()
+               |> String.split("<!-- MDOC -->")
+               |> Enum.fetch!(1)
+               |> String.trim()
+
   def project do
     [
       app: :battle_city_core,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
       elixir: "~> 1.11",
+      elixirc_options: [warnings_as_errors: System.get_env("CI") == "true"],
+      package: [
+        licenses: ["MIT"],
+        files: ["lib", ".formatter.exs", "mix.exs", "README*", "CHANGELOG*", "VERSION"],
+        exclude_patterns: ["priv/plts", ".DS_Store"],
+        links: %{
+          "GitHub" => @github_url,
+          "Changelog" => @github_url <> "/blob/master/CHANGELOG.md"
+        }
+      ],
+      docs: [
+        source_ref: "v" <> @version,
+        source_url: @github_url,
+        main: "readme",
+        extras: ["README.md", "CHANGELOG.md"]
+      ],
+      dialyzer: [
+        plt_core_path: "priv/plts",
+        plt_add_deps: :transitive,
+        plt_add_apps: [:ex_unit],
+        list_unused_filters: true,
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        flags: dialyzer_flags()
+      ],
+      preferred_cli_env: [ci: :test],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  defp dialyzer_flags do
+    [
+      :error_handling,
+      :race_conditions,
+      # :underspecs,
+      :unknown,
+      :unmatched_returns
+      # :overspecs
+      # :specdiffs
     ]
   end
 
@@ -21,8 +68,22 @@ defmodule BattleCityCore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      ci: [
+        "compile --warnings-as-errors --force --verbose",
+        "format --check-formatted",
+        "credo --strict",
+        "docs",
+        "dialyzer",
+        "test"
+      ]
     ]
   end
 end
