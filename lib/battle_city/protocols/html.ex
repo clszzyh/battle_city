dc0a51ef52d87d812c77c6b397bbe38e8fb5b3a5
clszzyh <clszzyh@gmail.com>
2020-12-25 12:11:29 +0800

    scale
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- lib/battle_city/protocols/html.ex
+++ lib/battle_city/protocols/html.ex
@@ -11,22 +11,19 @@ defprotocol Html do
 end
 
 defimpl Html, for: BattleCity.Environment do
-  @scale 32
   def grid(%{position: p, __module__: module}) do
-    %{type: :e, kind: module.__name__(), x: p.x * @scale, y: p.y * @scale}
+    %{type: :e, kind: module.__name__(), x: p.x, y: p.y}
   end
 end
 
 defimpl Html, for: BattleCity.Tank do
-  @scale 32
   def grid(%{position: p, __module__: module}) do
-    %{type: :t, kind: module.__name__(), x: p.x * @scale, y: p.y * @scale, d: p.direction}
+    %{type: :t, kind: module.__name__(), x: p.x, y: p.y, d: p.direction}
   end
 end
 
 defimpl Html, for: BattleCity.Bullet do
-  @scale 32
   def grid(%{position: p}) do
-    %{type: :b, x: p.x * @scale, y: p.y * @scale, d: p.direction}
+    %{type: :b, x: p.x, y: p.y, d: p.direction}
   end
 end
